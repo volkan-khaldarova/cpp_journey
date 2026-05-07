1bd7f27344400ec18013779494367bc7e603ff7f
Volkan Khaldarova <volkannecipoglu@proton.me>
2026-05-07 12:21:57 +0200

    feat: add program to display initials in block letters

--- 01_block_letters.cpp
+++ 01_block_letters.cpp
@@ -1,8 +1,19 @@
+/*A fun fact about me: I don't need to sleep, 
+ but I am constantly being updated to learn 
+ new information and skills! */
+
 #include <iostream>
 
 int main() {
-  
-  
+  // Displays the initials of my name in block letter.
+  std::cout << "\n";
+  std::cout << "V   V  K   K\n";
+  std::cout << "V   V  K  K \n";
+  std::cout << "V   V  K K  \n";
+  std::cout << "V   V  KK   \n";
+  std::cout << "V   V  K K  \n";
+  std::cout << " V V   K  K \n";
+  std::cout << "  V    K   K\n";  
   
   return 0;
-}
+} 
